6510841608c727c0a2bf90e738ecfb869bdce6cf
bobFisher1111 <rroseberry@twu.edu>
2025-08-20 19:11:17 -0500

    Removed Carousel, new version comming later

--- src/components/homePageComponent/HomePageComponent.tsx
+++ src/components/homePageComponent/HomePageComponent.tsx
@@ -1,30 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Button, Grid, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import ArticleList from "../articleList/ArticleList";
+import SeriesComponent from "../seriesList/SeriesComponent";
 import {
-  Button,
-  Grid,
-  Typography,
-} from '@mui/material';
-// import AddIcon from '@mui/icons-material/Add';
-// import RemoveIcon from '@mui/icons-material/Remove';
-import { useSelector } from 'react-redux';
-import ArticleList from '../articleList/ArticleList';
-import Carousel from '../carouselComponent/Carousel';
-import SeriesComponent from '../seriesList/SeriesComponent';
-import {
-  // CarouselGrid,
   GridRoot,
   LatestTextStyles,
-  // SeriesIconStyles,
   SeriesButtonStyles,
   SeriesTextStyles,
-} from './HomePageComponentStyles';
-import Sections from './Sections';
+} from "./HomePageComponentStyles";
+import Sections from "./Sections";
 
 
 const HomePageComponent: React.FC = () => {
   const getWebsiteData = useSelector((state: any) => state.webSiteData.data);
-  // const [numOfSeriesDisplayed, SetNumOfSeriesDisplayed] = useState<number>(4);
-  // const [seriesExpanded, SetSeriesExpanded] = useState<boolean>(false);
   const articleData = getWebsiteData && getWebsiteData?.articles;
   const authorsData = getWebsiteData && getWebsiteData?.authors;
   const seriesData = getWebsiteData && getWebsiteData?.series;
@@ -32,65 +21,33 @@ const HomePageComponent: React.FC = () => {
   const [series, setSeries] = useState<any>(seriesData);
 
   return (
-    <Grid
-      container
-      justifyContent='center'
-      alignItems="center"
-      sx={GridRoot}
-    >
-      {/* <Grid // breaking issue here
-        sx={CarouselGrid}
-      > */}
-      <Carousel data={articleData} author={authorsData} />
-      {/* </Grid> */}
-      <Grid
-        size={12}
-      >
-        <Sections data={articleData} series={seriesData} setData={setData} setSeries={setSeries} />
+    <Grid container justifyContent="center" alignItems="center" sx={GridRoot}>
+      <Grid size={12}>
+        <Sections
+          data={articleData}
+          series={seriesData}
+          setData={setData}
+          setSeries={setSeries}
+        />
       </Grid>
-      <Grid
-        size={12}
-      >
-        <Button
-          sx={SeriesButtonStyles}
-        >
-          <Typography
-            color="primary"
-            sx={SeriesTextStyles}
-          >
-            Series 
+      <Grid size={12}>
+        <Button sx={SeriesButtonStyles}>
+          <Typography color="primary" sx={SeriesTextStyles}>
+            Series
           </Typography>
-          {/* {!seriesExpanded ? 
-            <AddIcon
-              onClick={() => {
-                SetNumOfSeriesDisplayed(series.length);
-                return SetSeriesExpanded(true);
-              }}
-              sx={SeriesIconStyles}
-            /> 
-            : 
-            <RemoveIcon
-              sx={SeriesIconStyles}
-              onClick={() => {
-                SetNumOfSeriesDisplayed(4);
-                return SetSeriesExpanded(false);
-              }}
-            />
-          } */}
         </Button>
-        <SeriesComponent series={series?.slice(0, 4)} /> {/*  Come back later when redesign and fix this*/}
+        <SeriesComponent series={series?.slice(0, 4)} />
       </Grid>
-      <Grid
-        size={12}
-      >
-        <Typography
-          color="primary"
-          sx={LatestTextStyles}
-        >
+      <Grid size={12}>
+        <Typography color="primary" sx={LatestTextStyles}>
           Latest
         </Typography>
-        <ArticleList authorData={authorsData} data={data} turOnAuthorForArticle={false} />
-      </Grid> 
+        <ArticleList
+          authorData={authorsData}
+          data={data}
+          turOnAuthorForArticle={false}
+        />
+      </Grid>
     </Grid>
   );
 };
