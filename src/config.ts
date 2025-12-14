8a9153f12591eedc9a86d30689dfae91be3ebf34
bobFisher1111 <rroseberry@twu.edu>
2025-12-13 20:03:35 -0600

    Update to vite

--- src/config.ts
+++ src/config.ts
@@ -9,14 +9,14 @@ type AppConfig = {
   websiteHomePageImage: string;
 }
 
-export const appBaseURL = process.env.REACT_APP_APP_BASE_URL;
-export const apiBaseURL = process.env.REACT_APP_API_BASE_URL;
-export const facebookURL = process.env.REACT_APP_FACEBOOK_URL;
-export const youtubeURL = process.env.REACT_APP_YOUTUBE_URL;
-export const twitterURL = process.env.REACT_APP_TWITTER_URL;
-export const userAgreementValue = process.env.REACT_APP_USER_AGREEMENT_VALUE;
-export const websiteAPI = process.env.REACT_APP_WEBSITE_API_URL;
-export const websiteHomePageImage = process.env.REACT_APP_HOMEPAGE_IMAGE;
+export const appBaseURL = import.meta.env.VITE_APP_BASE_URL;
+export const apiBaseURL = import.meta.env.VITE_API_BASE_URL;
+export const facebookURL = import.meta.env.VITE_FACEBOOK_URL;
+export const youtubeURL = import.meta.env.VITE_YOUTUBE_URL;
+export const twitterURL = import.meta.env.VITE_TWITTER_URL;
+export const userAgreementValue = import.meta.env.VITE_USER_AGREEMENT_VALUE;
+export const websiteAPI = import.meta.env.VITE_WEBSITE_API_URL;
+export const websiteHomePageImage = import.meta.env.VITE_HOMEPAGE_IMAGE;
 
 export default {
   appBaseURL,
