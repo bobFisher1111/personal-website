4055590b5164586df2f33e6a23f6959c53e356a4
bobFisher1111 <rsr0986@gmail.com>
2026-02-16 21:40:12 -0600

    Removed unused files

--- src/services/getdata-services.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import { apiBaseURL, websiteAPI } from 'src/config';
-import { WebsiteData } from 'src/types/websiteData';
-import onSuccess from './onSuccess';
-
-export const getWebsiteData2 = () => axios.get<WebsiteData>(`${apiBaseURL}`).then(onSuccess);
-
-export const getWebsiteData = () => axios.get<WebsiteData>(`${websiteAPI}`).then(onSuccess);
