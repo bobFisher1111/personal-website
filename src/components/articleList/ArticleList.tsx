190cc5f73f38be4e188070952193ce7e6b9bf1ac
bobFisher1111 <rsr0986@gmail.com>
2026-02-21 20:17:25 -0600

    Refactor ComingSoon for ArticleList for author

--- src/components/articleList/ArticleList.tsx
+++ src/components/articleList/ArticleList.tsx
@@ -5,34 +5,34 @@ import ComingSoon from "src/components/comingSoon/ComingSoon";
 import formatDate from "src/util/formatDate";
 
 export const ArticleList = ({ authorData, data }: Props) => {
-  const noArticles = data?.length === 0;
   const newNonImmutableArray = data?.map((item: any) => item);
   const sortByDate = newNonImmutableArray?.sort(
     (a: any, b: any) => b.article_id - a.article_id,
   );
 
+  if (!data || data.length === 0) {
+    return <ComingSoon />;
+  };
+
   return (
     <Grid container>
-      {noArticles ? (
-        <ComingSoon />
-      ) : (
-        sortByDate?.map((item: any) => (
-          <Fragment key={item.article_id}>
-            <ArticleCard
-              articleId={item.article_id}
-              authorData={authorData}
-              articleMedia={item.cover_image_or_video}
-              authorId={item.author_id}
-              date={formatDate(item.published_date)}
-              section={item.section}
-              series={item.series_id}
-              seriesId={item.series_id}
-              subtitle={item.article_subtitle}
-              title={item.article_title}
-            />
-          </Fragment>
-        ))
-      )}
+      {sortByDate?.map((item: any) => (
+        <Fragment key={item.article_id}>
+          <ArticleCard
+            articleId={item.article_id}
+            authorData={authorData}
+            articleMedia={item.cover_image_or_video}
+            authorId={item.author_id}
+            date={formatDate(item.published_date)}
+            section={item.section}
+            series={item.series_id}
+            seriesId={item.series_id}
+            subtitle={item.article_subtitle}
+            title={item.article_title}
+          />
+        </Fragment>
+      ))
+    }
     </Grid>
   );
 };
